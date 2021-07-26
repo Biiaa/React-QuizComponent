dcdaa505ae542024abde72706fe9d3bdc312c34f
Biiaa <biiaa.1296@gmail.com>
2021-07-26 14:32:58 +0100

    New

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
-import Quiz from './Quiz.js'
-import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <Quiz />
-    )
-  }
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { quiz_position: 1 }
+    }
+    render() {
+        retturn(
+            <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
+        )
+    }
 }
 
-export default App
+export default Quiz
