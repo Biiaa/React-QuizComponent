0c91ec5eb52fb393c4dc4bfbd5ff79247736bec4
Biiaa <biiaa.1296@gmail.com>
2021-08-03 09:14:35 +0100

    run3

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,8 @@ class QuizQuestion extends Component {
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
-           
+
+            this.props.showNextQuestionHandler()
         }
 
     }
